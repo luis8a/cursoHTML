29a74342850c2a9cdc288c93434a204b4dd2f234
miguelmorao <miguelmorao68@gmail.com>
2015-06-06 17:08:23 -0430

    version 06.06.2015-1

--- javascriptcontacto.js
+++ javascriptcontacto.js
@@ -6,7 +6,7 @@ function validacion()
 		
 		if(minombre2.value=="")
 			{
-				minombre2.style.background="#FFDDDD";
+				minombre2.style.background='#FFDDDD';
 			}
 			
 		/*De lo contrario, verificamos si el campo "micorreo2" está en blanco,
@@ -15,7 +15,7 @@ function validacion()
 			
 		else if(micorreo2.value=="")
 			{
-				micorreo2.style.background="#FFDDDD";
+				micorreo2.style.background='#FFDDDD';
 			}
 			/*De lo contrario, verificamos si los campos "micelular2" y
 				"mitelfonofijo2" estan en blanco. si están en blanco
@@ -24,7 +24,7 @@ function validacion()
 				de telefono fijo para que sirva de alertaal usuario, de que
 				debe llenar alguno de estos campos*/
 				
-			else if(micelular2.value=="" && mitelefonofijo2.value0="")
+			else if(micelular2.value=="" && mitelefonofijo2.value=="")
 				{
 					micelular2.setCustomValidity('Inserte al menos un número de teléfono');
 					micelular2.style.background='#FFDDDD';
@@ -47,7 +47,7 @@ function cambiarrango()
 	/*Con la instrucción "var" declaramos una variable local llamada salida. Esta variable
 		va a referenciar al campo "rango" del formulario*/
 		
-		var salida=document.getElementById('rango');
+		var salida=document.getElementById("rango");
 	
 	/*Creamos otra variable local "calculo" y le asignamos el valor
 		del campo "miedad2" y le restamos "20" */
@@ -76,12 +76,12 @@ function iniciar()
 		se le asignan valores referenciado al elemento
 		por el "id" */
 		
-		minombre2=document.getElementById('minombre');
-		micorreo2=document.getElementById('micorreo');
-		micelular2=document.getElementById('micelular');
-		mitelfonofijo2=document.getElementById('mitelfonofijo');
-		miedad2=document.getElementById('miedad');
-		micomentario2=document.getElementById('micomentario');
+		minombre2=document.getElementById("minombre");
+		micorreo2=document.getElementById("micorreo");
+		micelular2=document.getElementById("micelular");
+		mitelfonofijo2=document.getElementById("mitelfonofijo");
+		miedad2=document.getElementById("miedad");
+		micomentario2=document.getElementById("micomentario");
 		
 	/*Se Agrega un Escuchador al evento "input", de los campos
 		minombre2, micorreo2, micelular2, y mitelfonofijo2.
@@ -98,13 +98,13 @@ function iniciar()
 		Cada vz que hay un "cambio" (change) en el campo
 		se llama a la función "cambiarrango" */
 		
-		miedad2.addEventListener('change', cambiarrango, false);
+		miedad2.addEventListener("change", cambiarrango, false);
 		
 	/*Se agrega un escucha al evento "input", del campo
 		micomentario2. Cuando el usuario hace un "input", es decir agrega
 		a la funcion "validacion" */
 		
-		micomentario2.addEventListener('input', validacion, false);
+		micomentario2.addEventListener("input", validacion, false);
 		validacion();
 	}
 	/*Cuando se llama al programa Javascript, esta es la
@@ -113,4 +113,4 @@ function iniciar()
 		el formulario, se llama a la función "iniciar"
 		que está mas arriba */
 		
-window.addEventListener('load', iniciar, false);
+window.addEventListener("load", iniciar, false);
